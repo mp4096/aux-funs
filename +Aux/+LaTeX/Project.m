968a09f14293b7e3d170fbac436e4b0d5c66ab73
mp4096 <mikhail.pak@tum.de>
2015-07-27 22:50:03 +0200

    WIP LaTeX project class
    * Added checks before adding documents (if the document is already in
    the list)
    * Redefined some properties as dependent
    * Added PrintTrim for LaTeX projects

--- +Aux/+LaTeX/Project.m
+++ +Aux/+LaTeX/Project.m
@@ -1,32 +1,35 @@
 classdef Project < Aux.KeyValueUtils.KeyValueMixin
+    
+    
     properties (SetAccess = protected, GetAccess = public)
         docList;
         projectPath;
-        figPathCompDir;
-        figPathCurrDir;
     end
     
     properties (Access = protected)
-        args = {' -interaction=nonstopmode'};
-        numComp = 2;
+        mainDocNum = 1;
+        args = {'-interaction=nonstopmode'};
+        numCmpl = 2;
         debugOn;
         echoArg;
+    end
+    
+    properties (Dependent, SetAccess = immutable, GetAccess = public)
+        figPathCmplDir;
+        figPathCurrDir;
+        docNames;
+        docNamesListed;
     end
     
     properties (Constant, Access = protected)
         figFolderName = 'figures';
     end
     
+    
     methods
         function obj = Project(rootPath, projectName)
             rootPath = Aux.FileHandling.FormatFolderPath(rootPath);
-            
-            obj.Set('echo', true);
-            obj.Set('debug', true);
-            
             obj.projectPath = [rootPath, projectName, '/'];
-            obj.figPathCurrDir = [obj.projectPath, obj.figFolderName, '/'];
-            obj.figPathCompDir = ['./', obj.figFolderName, '/'];
             
             if ~exist(obj.projectPath, 'dir')
                 mkdir(obj.projectPath);
@@ -35,6 +38,9 @@ classdef Project < Aux.KeyValueUtils.KeyValueMixin
             if ~exist(obj.figPathCurrDir, 'dir')
                 mkdir(obj.figPathCurrDir);
             end
+            
+            obj.Set('echo', true);
+            obj.Set('debug', true);
         end
         
         function delete(obj)
@@ -46,8 +52,14 @@ classdef Project < Aux.KeyValueUtils.KeyValueMixin
             arrayfun(@(d) d.Close, obj.docList);
         end
         
-        function newDoc = AddDocument(obj, docName)
-            newDoc = Aux.LaTeX.Document([obj.projectPath, docName]);
+        function newDoc = AddDocument(obj, newDocName)
+            nameMatches = strcmp(newDocName, obj.docNames);
+            
+            if any(nameMatches)
+                error('Document ''%s'' already exists!', newDocName);
+            end
+            
+            newDoc = Aux.LaTeX.Document([obj.projectPath, newDocName]);
             obj.docList = [obj.docList, newDoc];
         end
         
@@ -59,23 +71,24 @@ classdef Project < Aux.KeyValueUtils.KeyValueMixin
             obj.CloseAll;
             
             % Compile the main document
-            mainDocName = [obj.docList(1).filename, '.tex'];
+            mainDoc = obj.docList(obj.mainDocNum);
+            mainDocName = [mainDoc.filename, '.tex'];
             
-            compileCom{1} = 'pdflatex';
-            compileCom{2} = mainDocName;
-            compileCom{3} = ['-output-directory=', obj.projectPath];
-            compileCom = strjoin([compileCom, obj.args]);
+            cmplCom{1} = 'pdflatex';
+            cmplCom{2} = mainDocName;
+            cmplCom{3} = ['-output-directory=', obj.projectPath];
+            cmplCom = strjoin([cmplCom, obj.args]);
             
             if obj.debugOn
                 fprintf('Running pdfLaTeX...\n\t');
             end
-            for i = 1 : 1 : obj.numComp
+            for i = 1 : 1 : obj.numCmpl
                 if obj.debugOn
                     fprintf('%i... ', i);
                 end
                 % Start compilation
                 % Use '[~, ~]' to suppress output to MATLAB Command Window
-                [~, ~] = dos(compileCom, obj.echoArg{:});
+                [~, ~] = dos(cmplCom, obj.echoArg{:});
             end
             if obj.debugOn
                 fprintf('done!\n');
@@ -88,11 +101,46 @@ classdef Project < Aux.KeyValueUtils.KeyValueMixin
             copyfile(templatePath, tDoc.fullPath);
             tDoc.Reopen;
         end
+        
+        function latexStr = PrintTrim(obj, h, figureName, varargin)
+            filename = [obj.figPathCurrDir, figureName];
+            latexStr = [obj.figPathCmplDir, figureName];
+            Aux.FigureOperations.PrintTrim(h, filename, varargin{:});
+        end
+    end
+    
+    methods
+        function val = get.docNames(obj)
+            if ~isempty(obj.docList)
+                val = {obj.docList.filename};
+            else
+                val = [];
+            end
+        end
+        
+        function val = get.docNamesListed(obj)
+            if ~isempty(obj.docList)
+            str1 = {sprintf('Available documents:\n')};
+            prtFun = @(x) sprintf('\t''%s''\n', x);
+            str2 = cellfun(prtFun, obj.docNames, 'UniformOutput', false);
+            val = strjoin([str1, str2], '');
+            else
+                val = sprintf('No documents available.\n');
+            end
+        end
+        
+        function val = get.figPathCmplDir(obj)
+            val = ['./', obj.figFolderName, '/'];
+        end
+        
+        function val = get.figPathCurrDir(obj)
+            val = [obj.projectPath, obj.figFolderName, '/'];
+        end
     end
     
     methods (Hidden)
         function Set.num_pdflatex_runs(obj, val)
-            obj.numComp = val;
+            obj.numCmpl = val;
         end
         
         function Set.pdflatex_args(obj, val)
@@ -108,6 +156,20 @@ classdef Project < Aux.KeyValueUtils.KeyValueMixin
                 obj.echoArg = {'-echo'};
             else
                 obj.echoArg = {};
+            end
+        end
+        
+        function Set.main_document(obj, val)
+            nameMatches = strcmp(val, obj.docNames);
+            if ~any(nameMatches)
+                errStr{1} = sprintf('Document ''%s'' not found!', val);
+                errStr{2} = obj.docNamesListed;
+                error(strjoin(errStr));
+            else
+                obj.mainDocNum = find(nameMatches, 1);
+                if obj.debugOn
+                    fprintf('Document ''%s'' set as main!\n', val);
+                end
             end
         end
     end
