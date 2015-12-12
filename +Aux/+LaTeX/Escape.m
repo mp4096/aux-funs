4830cfcd3bf11d093db197d13a1da7ecbb14f0f2
mp4096 <mikhail.pak@tum.de>
2015-12-12 21:40:22 +0100

    Improved the LaTeX escape function
    Much better performance, especially for long strings. Now using strsplit and maps instead of char-wise comparison with lambda funs.

--- +Aux/+LaTeX/Escape.m
+++ +Aux/+LaTeX/Escape.m
@@ -13,6 +13,7 @@ function escStr = Escape(rawStr, escChrs)
 % Outputs:
 %   ret       : output string
 
+% Define the default escape combinations if no second argument
 if nargin == 1
     escChrs = { ...
         '%', '{\%}'; ...
@@ -27,18 +28,22 @@ if nargin == 1
         };
 end
 
-% 'compFun' compares a char ('c') with the list of special characters
-compFun = @(c) cellfun(@(x) x == c, escChrs(:, 1));
-% 'replFun' replaces a special char ('c') with its escaped counterpart
-replFun = @(c) escChrs{compFun(c), 2};
+% Create a map with the characters to be escaped as keys
+escMap = containers.Map(escChrs(:, 1), escChrs(:, 2), 'UniformValues', 1);
 
-% Get the indices of all special characters in the raw string
-idx = arrayfun(@(c) any(compFun(c)), rawStr);
-% Convert the raw string into a cell array of single chars (since an
-% escaped char can be longer than one character)
-escStr = num2cell(rawStr);
-% Replace the special chars
-escStr(idx) = cellfun(replFun, escStr(idx), 'UniformOutput', false);
+% Split the raw string into normal and special (i.e. to be escaped)
+% characters. Notice the 'CollapseDelimiters' option, it is mandatory here.
+[normalChars, specialChars] = ...
+    strsplit(rawStr, escChrs(:, 1), 'CollapseDelimiters', false);
+
+% Replace the special characters with the values from the map
+specialChars = values(escMap, specialChars);
+
+% Zip the escaped special and normal characters together. Pad the special
+% characters with a empty cell so that both arrays have the same length.
+escStr = [normalChars; [specialChars, {''}]];
+escStr = escStr(:).';
+
 % Join the cells into one string (without separators)
 escStr = strjoin(escStr, '');
 end
