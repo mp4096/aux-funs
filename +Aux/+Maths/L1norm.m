e469c7d81f5267d930de16dd80992ce0344f272e
niermeyer <philipp.niermeyer@tum.de>
2016-12-12 09:55:28 +0100

    L1 norm computation fixed

--- +Aux/+Maths/L1norm.m
+++ +Aux/+Maths/L1norm.m
@@ -1,19 +1,25 @@
 function normVal = L1norm(sys)
-% L1 norm of MIMO transfer function
+% L1 norm of MIMO transfer function (Peak to peak)
+% for the definition of the L1 norm, see nonlinear flight control lectures
+% Mathematical preliminaries: page 66 
 
-if ~isstable(sys)
-    error('System is not stable, L1 norm does not exist');
+if ~isstable(tf(sys))
+    error('System is not input/output stable, L1 norm does not exist');
 end
 
 % Find the slowest mode
 if sys.Ts == 0 % is it a continuous time system?
     omegaMin = min(abs(eig(sys)));
+    omegaMax = max(abs(eig(sys)));
 else % ... seems it's discrete
     omegaMin = min(abs(log(eig(sys))/sys.Ts));
+    omegaMax = max(abs(log(eig(sys))/sys.Ts));
 end
 
 % impulse response up to 100 * biggest time constant
-[y, t] = impulse(sys, 100/omegaMin);
+dT = 1e-3 / omegaMax;
+t = 0:dT:100/omegaMin; 
+[y, t] = impulse(sys, t);
 
 % integral of impulse responses absolute value
 impInt = trapz(t, abs(y));
